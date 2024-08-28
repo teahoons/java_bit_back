563ff0e37a14167747e06ffc2db8923e05bbc1f5
DESKTOP-D294HI6\user <dgvchnh@gmail.com>
2024-08-28 19:14:49 +0900

    no message

---
+++ src/main/java/com/example/java_bit_back/JavaBitBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_bit_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBitBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_bit_back/JavaBitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_bit_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
